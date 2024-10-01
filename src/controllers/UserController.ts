ea2fb70b6ac6c4200d74fd0adbdc41e26a70ae3b
valmir-borges <valmirjuniorvaladao@gmail.com>
2024-10-01 10:42:36 -0300

    Ajustes no Controller de Usuário

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,7 +11,7 @@ const saltRounds = 10;
 class UserController {
   public async index(req: Request, res: Response): Promise<Response> {
     let user = TokenHelper.User;
-    console.log(user);
+    console.log("Usuário encontrado:", user);
     if (!user) {
       return res.status(404).send({ message: "User not found!" });
     }
@@ -21,6 +21,7 @@ class UserController {
   public async userFunction(req: Request, res: Response): Promise<Response> {
     const token = req.body;
     let functionUser = TokenHelper.Role;
+    console.log("Função do usuário:", functionUser);
     if (!functionUser) {
       return res.status(404).send({ message: "Função nao encontrada" });
     }
@@ -44,37 +45,44 @@ class UserController {
       console.log("Dados recebidos:", req.body);
 
       if (!name || !email || !password || !phone || !cpf || !role || !address) {
+        console.log("Erro: Campos obrigatórios ausentes");
         return res
           .status(400)
           .json({ message: "Todos os campos são obrigatórios." });
       }
 
+      console.log("Validando o papel do usuário:", role);
       if (
-        role !== "diretor" ||
-        role !== "professor" ||
-        role !== "estudante" ||
-        role !== "coordenador" ||
-        role !== "inspetor" ||
-        role !== "limpeza" ||
-        role !== "cozinha" ||
+        role !== "diretor" &&
+        role !== "professor" &&
+        role !== "estudante" &&
+        role !== "coordenador" &&
+        role !== "inspetor" &&
+        role !== "limpeza" &&
+        role !== "cozinha" &&
         role !== "outro"
       ) {
+        console.log("Erro: Papel inválido");
         return res.status(400).json({ message: "Role inválida." });
       }
+      const classEntity = await Class.findById(IdClass);
+      const school = await School.findById(IdSchool);
 
-      if (IdSchool) {
-        if (!mongoose.Types.ObjectId.isValid(IdSchool)) {
-          return res.status(400).json({ message: "ID da escola inválido." });
-        }
+
+      if (!school) {
+        console.log("Erro: ID da escola inválido.");
+        return res.status(400).json({ message: "ID da escola inválido." });
+
       }
-      if (IdClass) {
-        if (!mongoose.Types.ObjectId.isValid(IdClass)) {
-          return res.status(400).json({ message: "ID da turma inválido." });
-        }
+      if (role == "estudante" && !classEntity) {
+        console.log("Turma inválida 2");
+        return res.status(400).json({ message: "ID da turma inválido." });
+
       }
 
       const existingUser = await User.findOne({ email });
       if (existingUser) {
+        console.log("Erro: Este email já está cadastrado.");
         return res
           .status(400)
           .json({ message: "Este email já está cadastrado." });
@@ -82,12 +90,14 @@ class UserController {
 
       const existingUserCPF = await User.findOne({ cpf });
       if (existingUserCPF) {
+        console.log("Erro: Este CPF já está cadastrado.");
         return res
           .status(400)
           .json({ message: "Este CPF já está cadastrado." });
       }
 
       const hashedPassword = await bcrypt.hash(password, saltRounds);
+      console.log("Senha hasheada com sucesso.");
 
       const newUser = new User({
         name,
@@ -107,30 +117,43 @@ class UserController {
         approved: false,
       });
 
+      console.log("Novo usuário criado:", newUser);
+
       const createdUser: UsersModel | undefined | null = await User.create(
         newUser
       );
+      console.log("Usuário criado com sucesso:", createdUser);
+
       if (createdUser && createdUser._id) {
         const userId = createdUser._id.toString();
-        const school = await School.findById(IdSchool);
+        console.log("ID do usuário criado:", userId);
+
         if (school) {
           school.pendingRequests.push(userId);
           await school.save();
+          console.log("Escola atualizada com o ID do usuário:", school);
         } else {
+          console.log("Erro: Escola não encontrada.");
           return res.status(404).json({ message: "Escola não encontrada." });
         }
-        const classEntity = await Class.findById(IdClass);
-        if (classEntity) {
-          classEntity.pendingRequests = classEntity.pendingRequests || [];
-          classEntity.pendingRequests.push(userId);
-          await classEntity.save();
-        } else {
-          return res.status(404).json({ message: "Turma não encontrada." });
+        if (createdUser.role === "estudante") {
+          if (classEntity) {
+            classEntity.pendingRequests = classEntity.pendingRequests || [];
+            classEntity.pendingRequests.push(userId);
+            await classEntity.save();
+            console.log("Turma atualizada com o ID do usuário:", classEntity);
+          } else {
+            console.log("Erro: Turma não encontrada.");
+            return res.status(404).json({ message: "Turma não encontrada." });
+          }
         }
+
       }
+
+      console.log("Gerando token para o usuário criado.");
       return await TokenHelper.createUserToken(createdUser, res);
     } catch (error) {
-      console.error(error);
+      console.error("Erro ao criar usuário:", error);
       return res.status(500).json({ message: "Erro ao criar usuário." });
     }
   }
@@ -139,6 +162,7 @@ class UserController {
     const { email, password } = req.body as UsersModel;
     let userFind: UsersModel | null = await User.findOne({ email: email });
     if (userFind) {
+      console.log("Usuário encontrado para login:", userFind);
       if (!email) {
         return res.status(422).json({ message: "O email é obrigatório!" });
       }
@@ -147,10 +171,12 @@ class UserController {
       }
       const checkPassword = await bcrypt.compare(password, userFind.password);
       if (!checkPassword) {
+        console.log("Erro: Senha inválida");
         return res.status(422).json({ message: "Senha inválida" });
       }
       return await TokenHelper.createUserToken(userFind, res);
     } else {
+      console.log("Erro: Não há usuário cadastrado com esse e-mail");
       return res
         .status(422)
         .json({ message: "Não há usuário cadastrado com esse e-mail" });
@@ -160,14 +186,20 @@ class UserController {
   public async approveUser(req: Request, res: Response): Promise<Response> {
     try {
       const { userId, IdSchool, IdClass } = req.body;
+      console.log("Aprovando usuário:", userId);
+
       const user = await User.findById(userId);
       if (!user) {
+        console.log("Erro: Usuário não encontrado.");
         return res.status(404).json({ message: "Usuário não encontrado." });
       }
+
       const school = await School.findById(IdSchool);
       if (!school) {
+        console.log("Erro: Escola não encontrada.");
         return res.status(404).json({ message: "Escola não encontrada." });
       }
+
       school.pendingRequests = school.pendingRequests.filter(
         (request) => request !== userId
       );
@@ -176,7 +208,8 @@ class UserController {
       if (user.role === "estudante") {
         const turma = await Class.findById(IdClass);
         if (!turma) {
-          return res.status(404).json({ message: "Escola não encontrada." });
+          console.log("Erro: Turma não encontrada.");
+          return res.status(404).json({ message: "Turma não encontrada." });
         }
 
         turma.pendingRequests = turma.pendingRequests?.filter(
@@ -195,11 +228,12 @@ class UserController {
       user.IdClass = user.role === "estudante" ? IdClass : "";
       await user.save();
 
+      console.log("Usuário aprovado com sucesso:", user);
       return res
         .status(200)
         .json({ message: "Usuário aprovado com sucesso.", user: user });
     } catch (error) {
-      console.error(error);
+      console.error("Erro ao aprovar usuário:", error);
       return res.status(500).json({ message: "Erro ao aprovar usuário." });
     }
   }
@@ -207,14 +241,17 @@ class UserController {
   public async rejectUser(req: Request, res: Response): Promise<Response> {
     try {
       const { userId, IdSchool } = req.body;
+      console.log("Rejeitando usuário:", userId);
 
       const user = await User.findById(userId);
       if (!user) {
+        console.log("Erro: Usuário não encontrado.");
         return res.status(404).json({ message: "Usuário não encontrado." });
       }
 
       const school = await School.findById(IdSchool);
       if (!school) {
+        console.log("Erro: Escola não encontrada.");
         return res.status(404).json({ message: "Escola não encontrada." });
       }
 
@@ -224,12 +261,13 @@ class UserController {
       await school.save();
 
       await User.findByIdAndDelete(userId);
+      console.log("Usuário rejeitado e removido com sucesso.");
 
       return res.status(200).json({
         message: "Solicitação do usuário rejeitada e usuário removido.",
       });
     } catch (error) {
-      console.error(error);
+      console.error("Erro ao rejeitar usuário:", error);
       return res.status(500).json({ message: "Erro ao rejeitar usuário." });
     }
   }
