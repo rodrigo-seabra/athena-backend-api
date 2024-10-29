44e853aa8f152e62491d3a976af206582834063d
valmir-borges <valmirjuniorvaladao@gmail.com>
2024-10-29 09:17:43 -0300

    Segunda rota para tarefas duesoon

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -75,6 +75,7 @@ class Routes {
     this.routes.get("/tasks/completed-user/:userId", UpdateStatusesMiddleware.update, TaskController.getAllCompleteByUser)
     this.routes.get("/tasks/delay/:userId", UpdateStatusesMiddleware.update, TaskController.getOverdueTasksByUser)
     this.routes.get("/tasks/duesoon/:userId", UpdateStatusesMiddleware.update, TaskController.getTasksDueSoonByUser)
+    this.routes.get("/tasks/duesoon-user/:userId", UpdateStatusesMiddleware.update, TaskController.getTasksDueSoonByUser)
     this.routes.get("/tasks/inprogress/:userId", UpdateStatusesMiddleware.update, TaskController.getPendingTasksByUser)
     this.routes.get("/tasks/usergrade/:userId", TaskController.getUserGrades)
 
